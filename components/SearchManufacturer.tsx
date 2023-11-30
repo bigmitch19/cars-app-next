6b767f4a3301a76153e6e518c7935fbc9592bc8f
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-11-30 10:38:16 +0000

    Update layout and add car logo to search manufacturer component

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,19 @@
+'use client'
 import { SearchManufacturerProps } from "@/types"
+import { Combobox, Transition } from "@headlessui/react"
+import Image from "next/image"
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
     return (
-        <div>SearchManufacturer</div>
+        <div className="search-manufacturer">
+            <Combobox >
+                <div className="relative w-full">
+                    <Combobox.Button className="absolute top-[14px]">
+                        <Image src="/car-logo.svg" className="ml-4" alt="Car logo" width={20} height={20} />
+                    </Combobox.Button>
+                </div>
+            </Combobox>
+        </div>
     )
 }
 
