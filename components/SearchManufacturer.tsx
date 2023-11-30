4b43876ba850e9cc78b4a40b64182331ef8a6ac4
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-11-30 10:58:20 +0000

    Added use client

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,7 @@
 'use client'
+import Image from "next/image"
 import { SearchManufacturerProps } from "@/types"
 import { Combobox, Transition } from "@headlessui/react"
-import Image from "next/image"
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
     return (
