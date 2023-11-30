1f4c2607680f46bd18e0cf21091285b0bea61edf
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-11-30 14:05:28 +0000

    Refactor SearchManufacturer component and update package.json

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,20 +1,68 @@
-'use client'
-import Image from "next/image"
-import { SearchManufacturerProps } from "@/types"
-import { Combobox, Transition } from "@headlessui/react"
+'use client';
+import Image from 'next/image';
+import { Fragment, useState } from 'react';
+import { SearchManufacturerProps } from '@/types';
+import { Combobox, Transition } from '@headlessui/react';
+import { manufacturers } from '@/constants';
 
-const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
-    return (
-        <div className="search-manufacturer">
-            <Combobox >
-                <div className="relative w-full">
-                    <Combobox.Button className="absolute top-[14px]">
-                        <Image src="/car-logo.svg" className="ml-4" alt="Car logo" width={20} height={20} />
-                    </Combobox.Button>
-                </div>
-            </Combobox>
-        </div>
-    )
-}
+const SearchManufacturer = ({
+	manufacturer,
+	setManufacturer
+}: SearchManufacturerProps) => {
+	const [query, setQuery] = useState('');
 
-export default SearchManufacturer
+	const filteredManufacturers =
+		query === ''
+			? manufacturers
+			: manufacturers.filter((item) => {
+					item
+						.toLowerCase()
+						.replace(/\s/g, '')
+						.includes(query.toLowerCase().replace(/\s/g, ''));
+			  });
+
+	return (
+		<div className="search-manufacturer">
+			<Combobox>
+				<div className="relative w-full">
+					<Combobox.Button className="absolute top-[14px]">
+						<Image
+							src="/car-logo.svg"
+							className="ml-4"
+							alt="Car logo"
+							width={20}
+							height={20}
+						/>
+					</Combobox.Button>
+					<Combobox.Input
+						className="search-manufacturer__input"
+						placeholder="Volkswagen"
+						displayValue={(manufacturer) => manufacturer}
+						onChange={(e) => setQuery(e.target.value)}
+					></Combobox.Input>
+
+					<Transition
+						as={Fragment}
+						leave="transition ease-in duration-100"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+						afterLeave={() => setQuery('')}
+					></Transition>
+
+					<Combobox.Options>
+						{filteredManufacturers.length === 0 && query !== '' && (
+							<Combobox.Option
+								value={query}
+								className="search-manufacturer__option"
+							>
+								Create "{query}"
+							</Combobox.Option>
+						)}
+					</Combobox.Options>
+				</div>
+			</Combobox>
+		</div>
+	);
+};
+
+export default SearchManufacturer;
