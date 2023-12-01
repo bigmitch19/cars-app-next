e818d12871ec6a7e4737448f0d432b98e89904c6
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-12-01 11:17:49 +0000

    Update sign-in page and hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,12 +18,12 @@ const Hero = () => {
 		<div className="hero">
 			<div className="flex-1 pt-36 padding-x">
 				<h1 className="hero__title">
-					Find, book, rent a car—quick and super easy!
+					Discover, reserve, and drive your dream car - fast and effortlessly!
 				</h1>
 
-				<p className="hero__subtitle">
-					Streamline your car rental experience with our effortless booking
-					process.
+				<p className="hero__subtitle ">
+					Revolutionize your car rental journey with our seamless reservation
+					system.
 				</p>
 
 				<Link href="/explore-cars">
