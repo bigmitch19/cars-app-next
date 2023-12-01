b013e8672a3a53ef1c4a6cc79dbc73f2c18ee22e
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-12-01 14:16:33 +0000

    Update dependencies and fix layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
-import "./globals.css";
+import './globals.css';
 
-import { Footer, NavBar } from "@components";
+import { Footer, NavBar } from '@components';
 
 export const metadata = {
-  title: "Car Hub",
-  description: "Discover world's best car showcase application",
+	title: 'Carmax',
+	description: "Discover world's best car showcase application"
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang='en'>
-      <body className='relative'>
-        <NavBar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+export default function RootLayout({
+	children
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className="relative">
+				<NavBar />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
