bd50e4fa06d2f21784eafb5f9aef4605c19e54e7
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-12-07 13:36:14 +0000

    Update authentication configuration

--- components/Account-dashboard.tsx
+++ components/Account-dashboard.tsx
@@ -1,4 +1,7 @@
-const AccountDashboard = () => {
+import { auth } from '@/auth';
+
+const AccountDashboard = async () => {
+	const session = await auth();
 	// Placeholder user data
 	const user = {
 		username: 'john_doe',
@@ -8,27 +11,28 @@ const AccountDashboard = () => {
 	};
 
 	return (
-		<div className="bg-gray-100 min-h-screen flex justify-center items-center">
-			<div className="bg-white p-8 rounded shadow-md">
-				<h1 className="text-2xl font-bold mb-4">Account Dashboard</h1>
-				<div className="mb-4">
-					<p className="text-gray-600">
-						<span className="font-bold">Username:</span> {user.username}
-					</p>
-				</div>
-				<div className="mb-4">
-					<p className="text-gray-600">
-						<span className="font-bold">Email:</span> {user.email}
-					</p>
-				</div>
-				<div className="mb-4">
-					<p className="text-gray-600">
-						<span className="font-bold">Plan:</span> {user.plan}
-					</p>
-				</div>
-				{/* Add more user details here */}
-			</div>
-		</div>
+		<>{JSON.stringify(session, null, 2)}</>
+		// <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+		// 	<div className="bg-white p-8 rounded shadow-md">
+		// 		<h1 className="text-2xl font-bold mb-4">Account Dashboard</h1>
+		// 		<div className="mb-4">
+		// 			<p className="text-gray-600">
+		// 				<span className="font-bold">Username:</span> {user.username}
+		// 			</p>
+		// 		</div>
+		// 		<div className="mb-4">
+		// 			<p className="text-gray-600">
+		// 				<span className="font-bold">Email:</span> {user.email}
+		// 			</p>
+		// 		</div>
+		// 		<div className="mb-4">
+		// 			<p className="text-gray-600">
+		// 				<span className="font-bold">Plan:</span> {user.plan}
+		// 			</p>
+		// 		</div>
+		// 		{/* Add more user details here */}
+		// 	</div>
+		// </div>
 	);
 };
 
