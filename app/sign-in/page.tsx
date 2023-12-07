d57cfde5673bcdbe2763ed7342840d70d443eaf4
bigmitch19 <116350035+bigmitch19@users.noreply.github.com>
2023-12-07 12:05:13 +0000

    Fix formatting and update login message

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useState } from 'react';
 
@@ -21,9 +21,7 @@ export default function Home() {
 					<div className="self-start hidden lg:flex flex-col text-gray-300">
 						<h1 className="my-3 font-semibold text-4xl">Welcome back</h1>
 						<p className="pr-3 text-sm opacity-75">
-							Lorem ipsum is placeholder text commonly used in the graphic,
-							print, and publishing industries for previewing layouts and visual
-							mockups
+							To keep connected with us please login with your personal info
 						</p>
 					</div>
 				</div>
@@ -85,40 +83,18 @@ export default function Home() {
 						</div>
 					</div>
 				</div>
+				<svg
+					className="absolute bottom-0 left-0"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 1440 320"
+				>
+					<path
+						fill="#fff"
+						fillOpacity="1"
+						d="M0,0L40,42.7C80,85,160,171,240,197.3C320,224,400,192,480,154.7C560,117,640,75,720,74.7C800,75,880,117,960,154.7C1040,192,1120,224,1200,213.3C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+					></path>
+				</svg>
 			</div>
-			<footer className="bg-transparent absolute w-full bottom-0 left-0 z-30">
-				<div className="container p-5 mx-auto flex items-center justify-between">
-					<div className="flex mr-auto">
-						<a
-							href="https://codepen.io/uidesignhub"
-							target="_blank"
-							title="codepen aji"
-							className="text-center text-gray-700 focus:outline-none"
-						>
-							<img
-								src="/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg"
-								alt="aji"
-								className="object-cover mx-auto w-8 h-8 rounded-full w-10 h-10"
-							/>
-							<p className="text-xl">
-								aji<strong>mon</strong>
-							</p>
-						</a>
-					</div>
-				</div>
-			</footer>
-			<svg
-				className="absolute bottom-0 left-0"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 1440 320"
-			>
-				<path
-					fill="#fff"
-					fillOpacity="1"
-					d="M0,0L40,42.7C80,85,160,171,240,197.3C320,224,400,192,480,154.7C560,117,640,75,720,74.7C800,75,880,117,960,154.7C1040,192,1120,224,1200,213.3C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-				></path>
-			</svg>
-			<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"></script>
 		</>
 	);
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,10 +38,11 @@ const Hero = () => {
 				<div className="hero__image">
 					<div className="p-28 bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500 h-[500px] w-[300px]  lg:h-[800px] lg:w-[600px] "></div>
 					<Image
-						src="./public/hero.webp"
+						src="./hero.webp"
 						alt="hero"
 						fill
 						className="object-contain"
+						placeholder="blur"
 					/>
 				</div>
 
